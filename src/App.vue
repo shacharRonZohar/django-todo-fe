a60f6e88c2014847d7004dc08e562c1c26dd16f9
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-04-03 12:26:12 +0300

    added styling

--- src/App.vue
+++ src/App.vue
@@ -4,76 +4,72 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="logo">Todo App</div>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/todos">Todos</RouterLink>
-    </nav>
-  </header>
-  <RouterView />
+  <div class="main-layout">
+    <header class="main-header">
+      <div class="logo">Todo App</div>
+      <nav class="main-nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/todos">Todos</RouterLink>
+      </nav>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
+<style scoped lang="scss">
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 23px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+.main-layout {
+  display: grid;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  // place-items: center;
+  /* minheight: 100vh; */
+  height: 100%;
+  grid-template: 100px 1fr / minmax(20px, 1fr) 1000px minmax(20px, 1fr);
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  > * {
+    grid-column: 2;
+    // grid-row: 1;
   }
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.main-header {
+  line-height: 1.5;
+  // max-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.main-nav {
+  display: flex;
+  font-size: 19px;
+  margin: auto 0;
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+    &:first-of-type {
+      border: 0;
+    }
+
+    &:last-of-type {
+      padding-right: 0;
+    }
+
+    &.router-link-exact-active:hover {
+      background-color: transparent;
+    }
+    &.router-link-exact-active {
+      color: var(--color-text);
+    }
   }
 }
 </style>
